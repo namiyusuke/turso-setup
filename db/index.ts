ff9fdbaa27461a9d3b3147bb1d15b49d9c138cfe
namikawayusuke <yu.namikawa18@gmail.com>
2025-09-28 12:52:40 +0900

    fa

---
+++ db/index.ts
@@ -0,0 +1,13 @@
+import { drizzle } from "drizzle-orm/libsql/web";
+// まだ存在しない場合コメントアウト
+import * as authSchema from "./schemas/auth";
+
+export const db = drizzle({
+  connection: {
+    url: process.env.TURSO_DATABASE_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN!,
+  },
+  schema: {
+    // ...authSchema,
+  },
+});
